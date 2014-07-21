0c539191f77dc0ac87602f0d3b7fbebbc10a901a
YenHuang <yenhuang@lawn-128-61-119-251.lawn.gatech.edu>
2014-07-21 12:26:36 -0400

    doing the sign in page

--- js/home.js
+++ js/home.js
@@ -1,12 +1,38 @@
+// var $container = $('<div id = "container4"></div>');
+// var $caption = $("<p></p>");
+// $container.append($caption);
+// $container.append($caption);
+// $("body").append($container);
+
+
+
+
+
+
 $(document).ready(function(){
 	$("#signIn").click(function(){
-		document.getElementById("motto").innerHTML = "hello";
+		$(".container1, .container2").slideUp(500);
+
+var $container = $('<div id = "container4"></div>');
+var $form = $('<form method = "post"></form>');
+var $input = $('<input type = "text" name = "first name">');
+var $username = $('<p id = "username">USERNAME</p>');
+$form.append($username);
+$form.append($input);
+$container.append($form);
+$("body").append($container);
+
+
+		// $container.slideDown();
+		// $caption.text("hello");
+
+		
+		
+
+
     
 	});
 
 });
 
-// function test() {
-// 	document.getElementById("motto").fadeOut("fast");
-// }
 
